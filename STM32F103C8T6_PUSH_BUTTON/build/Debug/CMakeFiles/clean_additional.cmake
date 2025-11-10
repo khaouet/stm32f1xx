f65db1b3b830ea3187e56a9ef221cf70126cefee
khaouet <khaled.haouet@gmail.com>
2025-11-10 10:16:46 +0100

    feat(stm32f1xx) add project LEDs and ReadPin

--- STM32F103C8T6_PUSH_BUTTON/build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "STM32F103C8T6_PUSH_BUTTON.map"
-  )
-endif()
