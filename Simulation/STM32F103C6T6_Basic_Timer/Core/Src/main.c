b3969ede60da81163ada4422db940ce329ea4930
Khaled HAOUET <khaled.haouet@gmail.com>
2026-04-19 22:36:39 +0100

    feat(simulation) STM32F103C6T6_Basic_Timer

--- Simulation/STM32F103C6T6_Basic_Timer/Core/Src/main.c
+++ Simulation/STM32F103C6T6_Basic_Timer/Core/Src/main.c
@@ -161,12 +161,15 @@ static void MX_TIM3_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
   /* USER CODE BEGIN TIM3_Init 1 */
-
+  //
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 7199;
+  htim3.Init.Prescaler = (7200 - 1); // 10 kHz
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 9999;
+  // htim3.Init.Period = (1000 - 1); // 10Hz 100ms
+  // htim3.Init.Period = (100 - 1); // 100Hz 10ms
+  htim3.Init.Period = (10 - 1); // 1000Hz
+  //htim3.Init.Period = (10000 - 1); // 1Hz
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
